491a1050cac635ef02ae343f1ffe2d1338094db1
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2022-05-11 08:47:22 +0200

    Added JSDoc for json export

--- esm/json.js
+++ esm/json.js
@@ -6,6 +6,16 @@ import {serialize} from './serialize.js';
 const {parse: $parse, stringify: $stringify} = JSON;
 const options = {json: true, lossy: true};
 
+/**
+ * Revive a previously stringified structured clone.
+ * @param {string} str previously stringified data as string.
+ * @returns {any} whatever was previously stringified as clone.
+ */
 export const parse = str => deserialize($parse(str));
 
-export const stringify = str => $stringify(serialize(str, options));
+/**
+ * Represent a structured clone value as string.
+ * @param {any} any some clone-able value to stringify.
+ * @returns {string} the value stringified.
+ */
+export const stringify = any => $stringify(serialize(any, options));
