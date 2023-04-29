bc44074dea5fab74b07a97bde38bf1cf5bab2c39
Andrew Pyle <andrew.pyle@icloud.com>
2023-04-28 21:24:29 -0500

    Generate CJS .d.ts files from ESM .d.ts files

--- cjs/index.js
+++ cjs/index.js
@@ -2,16 +2,6 @@
 const {deserialize} = require('./deserialize.js');
 const {serialize} = require('./serialize.js');
 
-/**
- * Polyfilled structuredClone function.
- *
- * @note If only standard options are provided (`transfer` is standard,
- * while `json` is not), and If a native implementation of `structuredClone()`
- * is available at the time of invocation, the native implementation is
- * used instead of this module's code. In this case, The `transfer`
- * property in the `options` parameter is not supported—all values
- * are always cloned.
- */
 Object.defineProperty(exports, '__esModule', {value: true}).default = typeof structuredClone === "function" ?
   /* c8 ignore start */
   (any, options) => (
--- cjs/json.js
+++ cjs/json.js
@@ -9,16 +9,16 @@ const options = {json: true, lossy: true};
 
 /**
  * Revive a previously stringified structured clone.
- * @param {string} str previously stringified data as string.
- * @returns {any} whatever was previously stringified as clone.
+ * @param str previously stringified data as string.
+ * @returns whatever was previously stringified as clone.
  */
 const parse = str => deserialize($parse(str));
 exports.parse = parse;
 
 /**
  * Represent a structured clone value as string.
- * @param {any} any some clone-able value to stringify.
- * @returns {string} the value stringified.
+ * @param any some clone-able value to stringify.
+ * @returns the value stringified.
  */
 const stringify = any => $stringify(serialize(any, options));
 exports.stringify = stringify;
